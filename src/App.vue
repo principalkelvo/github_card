2c0638d9ce2df2a3fa58f7a60c4b9079f9d9e550
principal <kelvinnjoroowino@gmail.com>
2024-06-02 23:10:45 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto">
+    <GithubCard/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GithubCard from "./components/GithubCard.vue";
+</script>
+
+<style scoped>
+
+</style>
